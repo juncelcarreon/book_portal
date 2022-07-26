3a53456f857eddf3b797ba4478c79d9ac42f129b
June Cadayona <junecadayona@elink.corp>
2022-07-26 22:57:55 +0800

    Organize web route

--- routes/web.php
+++ routes/web.php
@@ -24,28 +24,35 @@ Route::middleware(['guest'])->group(function () {
 });
 
 Route::middleware('auth')->group(function(){
+
     Route::get('/home', function(){
         return redirect(route('dashboard'));
     });
 
-    Route::get('/', [AuthenticationController::class, 'dashboard'])->name('dashboard');
-    Route::post('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+    Route::controller(AuthenticationController::class)->group(function(){
+        Route::get('/', 'dashboard')->name('dashboard');
+        Route::post('/logout', 'logout')->name('logout');
+    });
 
-    Route::get('/authors', [AuthorController::class, 'index'])->name('author.index');
-    Route::get('/authors/import', [AuthorController::class, 'importPage'])->name('author.import-page');
-    Route::post('/authors/import', [AuthorController::class, 'import'])->name('author.import-bulk');
-    Route::get('/authors/create', [AuthorController::class, 'create'])->name('author.create');
-    Route::get('/authors/{author}', [AuthorController::class, 'edit'])->name('author.edit');
-    Route::post('/authors/create', [AuthorController::class, 'store'])->name('author.store');
-    Route::put('/authors/{author}', [AuthorController::class, 'update'])->name('author.update');
-    Route::delete('/authors/{author}', [AuthorController::class, 'delete'])->name('author.delete');
+    Route::controller(AuthorController::class)->group(function(){
+        Route::get('/authors', 'index')->name('author.index');
+        Route::get('/authors/import', 'importPage')->name('author.import-page');
+        Route::post('/authors/import', 'import')->name('author.import-bulk');
+        Route::get('/authors/create', 'create')->name('author.create');
+        Route::get('/authors/{author}', 'edit')->name('author.edit');
+        Route::post('/authors/create', 'store')->name('author.store');
+        Route::put('/authors/{author}', 'update')->name('author.update');
+        Route::delete('/authors/{author}', 'delete')->name('author.delete');
+    });
 
-    Route::get('/books', [BookController::class, 'index'])->name('book.index');
-    Route::get('/books/import', [BookController::class, 'importPage'])->name('book.import-page');
-    Route::post('/books/import', [BookController::class, 'import'])->name('book.import-bulk');
-    Route::get('/books/create', [BookController::class, 'create'])->name('book.create');
-    Route::get('/books/{book}', [BookController::class, 'edit'])->name('book.edit');
-    Route::post('/books/create', [BookController::class, 'store'])->name('book.store');
-    Route::put('/books/{book}', [BookController::class, 'update'])->name('book.update');
-    Route::delete('/books/{book}', [BookController::class, 'delete'])->name('book.delete');
+    Route::controller(BookController::class)->group(function(){
+        Route::get('/books','index')->name('book.index');
+        Route::get('/books/import','importPage')->name('book.import-page');
+        Route::post('/books/import', 'import')->name('book.import-bulk');
+        Route::get('/books/create', 'create')->name('book.create');
+        Route::get('/books/{book}', 'edit')->name('book.edit');
+        Route::post('/books/create', 'store')->name('book.store');
+        Route::put('/books/{book}', 'update')->name('book.update');
+        Route::delete('/books/{book}', 'delete')->name('book.delete');
+    });
 });
