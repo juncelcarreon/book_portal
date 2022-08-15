7c34fdd50c8426ff68411fc353b34d745001a3f5
Ferlyn Ruiz <ferlynruiz@elink.corp>
2022-08-16 06:33:13 +0800

    Change pod and ebook royalty number format

--- app/Http/Controllers/GeneratePdfController.php
+++ app/Http/Controllers/GeneratePdfController.php
@@ -55,11 +55,11 @@ class GeneratePdfController extends Controller
                     if($podFirst){
                         if($podFirst->format == 'Hardback'){
                             $quantity = $podTransactions->where('year', $year)->where('month', $month)->where('format', 'Hardback')->sum('quantity');
-                            $royalty = $podTransactions->where('year', $year)->where('month', $month)->where('format', 'Hardback')->sum('royalty');
+                            $royalty = number_format((float)$podTransactions->where('year', $year)->where('month', $month)->where('format', 'Hardback')->sum('royalty'), 2);
                             $pods->push(['title' => $podFirst->book->title, 'year' => $year, 'month' => $month, 'format' => 'Hardback', 'quantity' => $quantity, 'price' => $podFirst->price, 'royalty' => $royalty]);
                         }else{
                             $quantity = $podTransactions->where('year', $year)->where('month', $month)->where('format', 'Paperback')->sum('quantity');
-                            $royalty = $podTransactions->where('year', $year)->where('month', $month)->where('format', 'Paperback')->sum('royalty');
+                            $royalty = number_format((float)$podTransactions->where('year', $year)->where('month', $month)->where('format', 'Paperback')->sum('royalty'), 2);
                             $pods->push(['title' => $podFirst->book->title, 'year' => $year, 'month' => $month, 'format' => 'Paperback', 'quantity' => $quantity, 'price' => $podFirst->price, 'royalty' => $royalty]);
                         }
                     }
@@ -90,7 +90,7 @@ class GeneratePdfController extends Controller
                 $ebook = $ebookTransactions->where('year', $year)->where('month', $month)->first();
                 if($ebook){
                     $quantity = $ebookTransactions->where('year', $year)->where('month', $month)->sum('quantity');
-                    $royalty = $ebookTransactions->where('year', $year)->where('month', $month)->sum('royalty');
+                    $royalty = number_format((float)$ebookTransactions->where('year', $year)->where('month', $month)->sum('royalty'), 2);
                     $ebooks->push(['title' => $ebook->book->title, 'year' => $year, 'month' => $month,'quantity' => $quantity, 'price' => $ebook->price, 'royalty' => $royalty]);
                 }
             }
@@ -98,21 +98,23 @@ class GeneratePdfController extends Controller
         $totalPOD = [];
         $totalPOD['title']= $podTransactions[0]->book->title ." Total";
         $totalPOD['quantity'] = $podTransactions->sum('quantity');
-        $totalPOD['royalty'] = $podTransactions->sum('royalty');
+        $totalPOD['royalty'] = number_format((float)$podTransactions->sum('royalty'), 2);
         $totalPOD['price'] = $podTransactions[0]->price;
 
 
         $totalEbook = [];
         $totalEbook['title'] = $ebookTransactions[0]->book->title .' Total';
         $totalEbook['quantity'] = $ebookTransactions->sum('quantity');
-        $totalEbook['royalty'] = $ebookTransactions->sum('royalty');
+        $totalEbook['royalty'] = number_format((float)$ebookTransactions->sum('royalty'), 2);
         $totalEbook['price'] = $ebookTransactions[0]->price;
 
         // pods, ebooks, totalPOD, totalEbook
 
         return view('report.pdf', [
             'pods' => $pods,
-            'ebooks' => $ebooks
+            'ebooks' => $ebooks,
+            'totalPOD' => $totalPOD,
+            'totalEbook' => $totalEbook
         ]);
         // $pdf = PDF::loadView('report.pdf');
         // return $pdf->download('file.pdf');
